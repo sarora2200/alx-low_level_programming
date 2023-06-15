6b1394ccb70d8c73b0a09387da32ea16a52f3cf7
sarora2200 <sarora2200@gmail.com>
2023-06-15 09:12:41 -0700

    fix

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{ char (c) = 0;
+int (i) = 0;
+long int (l) = 00000;
+long long int (ll) = 00000000000000000000
+float (f) = 0.00;
+printf("size of a char is %ld byte(s)\n", sizeof(char));
+printf("size of a int is %ld byte(s)\n", sizeof(int));
+printf("size of a long int is %ld byte(s)\n", sizeof(long int));
+printf("size of a long long int is %ld byte(s)\n", sizeof(long long int));
+printf("size of a float is %ld byte(s)\n", sizeof(float));
+
+return (0);
+}
+
