4f888d2edef0f268657b5a06722f2df2a76425e5
sarora2200 <sarora2200@gmail.com>
2023-06-15 10:27:28 -0700

    done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %u byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
-printf("size of a float: %u byte(s)\n", sizeof(float));
+printf("Size of a float: %u byte(s)\n", sizeof(float));
 return (0);
 }
