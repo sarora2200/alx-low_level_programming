8f36262353dd3e668b6a344687c9dee3f8f51315
sarora2200 <sarora2200@gmail.com>
2023-06-15 10:46:45 -0700

    new

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 1 (Success)
+*/
+
+int main(void)
+{ echo "Programming is like building a multilingual puzzle";
+return (1);
