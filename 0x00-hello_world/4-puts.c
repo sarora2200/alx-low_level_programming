1d0fc27f5af05fd511fb67ebc84b7b631ca58622
sarora2200 <sarora2200@gmail.com>
2023-06-15 06:04:08 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
-#!/bin/bash 
 #include <stdio.h>
-int main (){
-puts ("Programming is like building a multilingual puzzle");
-return 0;
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{ puts("Programming is like building a multilingual puzzle");
+return (0);
 }
