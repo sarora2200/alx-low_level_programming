cc9614b96a59a5ff473090589af578e3bb9a38a3
sarora2200 <sarora2200@gmail.com>
2023-06-16 09:13:54 -0700

    done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{ char ch = 'a';
+while (ch <= 'z')
+{
+if (ch == 'e' || ch == 'q')
+ch++;
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
