8ee2f07a2dc76793ec34b7258224a08ae9a8651c
sarora2200 <sarora2200@gmail.com>
2023-06-16 11:55:47 -0700

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * description: prints all combinations between single digits
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{ int i = 0
+for (i = 0; i <= 9; i++)
+{putchar(i + 48);
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n')
+return (0);
+}
