831ba411b8fa670d03ced1138d133268889a32d0
sarora2200 <sarora2200@gmail.com>
2023-06-20 06:23:19 -0700

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+*/
+
+void print_alphabet(void)
+{ char b;
+for (b = 'a'; b <= 'z'; b++)
+_putchar('b');
+_putchar('\n');
+}
+
