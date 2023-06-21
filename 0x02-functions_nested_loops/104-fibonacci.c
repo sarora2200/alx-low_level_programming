a39d2d8e429a77aa129dd8b5c4e06e5a483d3111
sarora2200 <sarora2200@gmail.com>
2023-06-20 19:17:40 -0700

    done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,7 +17,8 @@ for (count = 1; count <= 98; count++)
 {
 if (f1o > 0)
 printf("%lu", f1o);
-initial0s = numlength - 1 - numlength(f1o);
+initial0s = numlength - 1 - 
+numlength * f1o;
 while (f1o > 0 && initial0s > 0)
 {
 printf("%d", 0);
