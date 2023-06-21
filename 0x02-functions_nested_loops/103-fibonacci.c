60bee411ca963070dc66417a2b9b487e035f40aa
sarora2200 <sarora2200@gmail.com>
2023-06-20 17:56:33 -0700

    done

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -15,7 +15,7 @@ unsigned long fib1 = 0, fib2 = 1, sum;
 for (count =0; count < 50; count++)
 {
 sum = fib1 + fib2;
-print("%lu", sum);
+printf("%lu", sum);
 fib1 = fib2;
 fib2 = sum;
 if (count == 49)
