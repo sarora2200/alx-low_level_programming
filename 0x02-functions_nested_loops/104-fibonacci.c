3ca5bab7ff066a050c7986286a8abddd9c6609d8
sarora2200 <sarora2200@gmail.com>
2023-06-20 19:20:50 -0700

    done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,8 +17,8 @@ for (count = 1; count <= 98; count++)
 {
 if (f1o > 0)
 printf("%lu", f1o);
-initial0s = numlength - 1 - 
-numlength * f1o;
+initial0s = numlength - 1 -
+numlength *f1o;
 while (f1o > 0 && initial0s > 0)
 {
 printf("%d", 0);
@@ -26,7 +26,7 @@ initial0s--;
 }
 printf("%lu", f1);
 sum = (f1 + f2) % mx;
-sumo = f1o + f2o + (f1 +f2) / mx;
+sumo = f1o + f2o + (f1 + f2) / mx;
 f1 = f2;
 f1o = f2o;
 f2 = sum;
