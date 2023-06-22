fc93a7c6bb35c8a7782fb6536022c151ecad08f5
sarora2200 <sarora2200@gmail.com>
2023-06-22 09:41:03 -0700

    done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,15 +18,15 @@ int m = n % 3;
 int o = n % 5;
 if (m == 0 && o != 0)
 {
-printf("Fizz ");
+printf(" Fizz");
 }
 else if (o == 0 && m != 0)
 {
-printf("Buzz ");
+printf(" Buzz");
 }
 else if (m == 0 && o == 0)
 {
-printf("FizzBuzz ");
+printf(" FizzBuzz");
 }
 else
 {
