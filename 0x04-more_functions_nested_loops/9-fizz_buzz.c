6462a904a606c44614119fe37b400e454e468ae2
sarora2200 <sarora2200@gmail.com>
2023-06-22 09:50:03 -0700

    done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,17 @@ int main(void)
 int n;
 for (n = 1; n <= 100; n++)
 {
-if (n % 3 == 0 && n % 5 != 0)
+int m = n % 3;
+int o = n % 5;
+if (m == 0 && o != 0)
 {
 printf(" Fizz");
 }
-else if (n % 5 == 0 && n % 3 != 0)
+else if (o == 0 && m != 0)
 {
 printf(" Buzz");
 }
-else if (n % 3 == 0 && n % 5 == 0)
+else if (m == 0 && o == 0)
 {
 printf(" FizzBuzz");
 }
