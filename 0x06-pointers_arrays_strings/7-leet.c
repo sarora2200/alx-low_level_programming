6e8658295cc288a906c7cb3d22378b68fd491cb6
sarora2200 <sarora2200@gmail.com>
2023-06-29 16:39:39 -0700

    done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,18 +7,18 @@
 char *leet(char *str)
 {
 int i;
+int j;
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711";
 for (i = 0; str[i] != '\0'; i++)
 {
-if (str[i] == 'a' || str[i] == 'A')
-str[i] = '4';
-else if (str[i] == 'e' || str[i] == 'E')
-str[i] = '3';
-else if (str[i] == 'o' || str[i] == 'O')
-str[i] = '0';
-else if (str[i] == 't' || str[i] == 'T')
-str[i] = '7';
-else if (str[i] == 'l' || str[i] == 'L')
-str[i] = '1';
+for (j = 0; j < 10; j++)
+{
+if (str[i] == s1[j])
+{
+str[i] = s2[j];
+}
+}
 }
 return (str);
 }
