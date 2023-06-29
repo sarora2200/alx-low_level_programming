8f74bed75b6079c489ded6fbd3e8b15f6ddec222
sarora2200 <sarora2200@gmail.com>
2023-06-29 12:05:28 -0700

    done

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: input string.
+ * @s2: input string.
+ * Return: the result 0 is the same, less than 0 if taller, more than 0 
+ * if smaller.
+*/
+int _strcmp(char *s1, char *s2)
+{
+int i;
+for (i = 0; s1[i] == s2[i]; i++)
+{
+return (0);
+}
+return s1[i] - s2[i];
+}
