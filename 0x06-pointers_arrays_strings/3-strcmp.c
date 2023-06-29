02ffc20ec34a195b74a3057d402e0660c81bc1ae
sarora2200 <sarora2200@gmail.com>
2023-06-29 12:11:15 -0700

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
  * _strcmp - function that compares two strings.
  * @s1: input string.
  * @s2: input string.
- * Return: the result 0 is the same, less than 0 if taller, more than 0 
+ * Return: the result 0 is the same, less than 0 if taller, more than 0
  * if smaller.
 */
 int _strcmp(char *s1, char *s2)
@@ -14,5 +14,5 @@ for (i = 0; s1[i] == s2[i]; i++)
 if (s1[i] == '\0')
 return (0);
 }
-return s1[i] - s2[i];
+return (s1[i] - s2[i]);
 }
