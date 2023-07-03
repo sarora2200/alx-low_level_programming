8f2877e2f5a5d6350e985c816413e620c0061104
sarora2200 <sarora2200@gmail.com>
2023-07-03 12:13:54 -0700

    done

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,8 +12,12 @@ int i;
 int e;
 for (i = 0; haystack[i] != '\0'; i++)
 {
-for (e = 0; needle[e] != '\0' && haystack[i + e] == needle[e]; e++)
-if (needle[e + 1] == '\0')
+for (e = 0; needle[e] != '\0'; e++)
+{
+if (haystack[i + e] != needle[e])
+break;
+}
+if (needle[e] == '\0')
 return (haystack + i);
 }
 return ('\0');
