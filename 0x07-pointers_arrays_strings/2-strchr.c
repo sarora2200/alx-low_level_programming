11a78d4fa31f41b9bff68e9cc08e8fe69efdb311
sarora2200 <sarora2200@gmail.com>
2023-07-03 09:00:39 -0700

    done

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,7 @@
  * _strchr - function that locates a character in a string.
  * @s: is an input string.
  * @c: is a charachter to be found.
- * Return: pointer to the first occurrence of the character c in the string s 
+ * Return: pointer to the first occurrence of the character c in the string s
  * or NULL if the character is not found.
 */
 char *_strchr(char *s, char c)
