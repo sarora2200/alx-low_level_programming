7b9d1be81f2c9a49b5485e512cedea06c9f5347c
sarora2200 <sarora2200@gmail.com>
2023-07-07 03:45:55 -0700

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * @argc: int.
+ * @argv: string.
+ * Return: 0 is (success).
+*/
+
+int main(int argc, char const *argv[])
+{
+(void)argv;
+printf("%i\n", argc - 1);
+return (0);
+}
