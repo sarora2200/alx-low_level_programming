67dcb3e59990d393db488b73c35b47bcbc3490bd
sarora2200 <sarora2200@gmail.com>
2023-07-13 06:51:06 -0700

    done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,38 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+
 /**
- * *string_nconcat - function that concatenates two strings.
- * @s1: input string.
- * @s2: input string.
- * @n: input int.
- * Return: pointer point to a newly allocated space in memory.
+* string_nconcat - function that concatenates two strings.
+* @s1: input string.
+* @s2: input string.
+* @n: input int.
+* Return: pointer to a newly allocated space in memory.
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s3;
 unsigned int len1, len2, len3, i, j;
+
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
+
 for (len1 = 0; s1[len1] != '\0'; len1++)
-	;
+;
 for (len2 = 0; s2[len2] != '\0'; len2++)
-	;
+;
+
 if (n >= len2)
 len3 = len1 + len2;
 else
-len 3 = len1 + n;
+len3 = len1 + n;
+
 s3 = malloc((len3 + 1) * sizeof(char));
 if (s3 == NULL)
-{
 return (NULL);
-}
+
 for (i = 0; i < len1; i++)
-{
 s3[i] = s1[i];
-}
+
 for (j = 0; j < n && s2[j]; j++)
 {
 s3[i] = s2[j];
