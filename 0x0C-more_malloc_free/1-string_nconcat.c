a90cf60840b37d614f2589f4b518a9dbf582cfdd
sarora2200 <sarora2200@gmail.com>
2023-07-13 06:30:30 -0700

    done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,16 +12,13 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s3;
-unsigned int len1 = 0;
-unsigned int len2 = 0;
-unsigned int len3 = 0;
-unsigned int i, j;
+unsigned int len1, len2, i, j;
 if (s1 == NULL)
-return ("");
+s1 = "";
 if (s2 == NULL)
-return ("");
-for (len1 = 0; s1[len1]; len1++)
-for (len2 = 0; s2[len2]; len2++)
+s2 = "";
+for (len1 = 0; s1[len1] != '\0'; len1++)
+for (len2 = 0; s2[len2] != '\0'; len2++)
 if (n >= len2)
 len3 = len1 + len2;
 else
