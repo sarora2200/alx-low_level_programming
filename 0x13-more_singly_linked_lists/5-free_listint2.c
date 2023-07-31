d0328f65d77aa3aba2e223ee25c7498bbabfdc43
sarora2200 <sarora2200@gmail.com>
2023-07-31 11:43:13 -0700

    done

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * free_listint - function that frees a listint_t list.
+ * free_listint2-function that frees a listint_t list.
  * @head: pointer to first node.
 */
 void free_listint2(listint_t **head)
