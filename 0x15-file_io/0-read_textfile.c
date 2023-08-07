ebb1051e6095a7160f7b5a75954d62b5285f5270
sarora2200 <sarora2200@gmail.com>
2023-08-07 10:58:30 -0700

    done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,7 @@
  * to the POSIX standard output.
  * @filename: the filename
  * @letters: the number of letters it should read and print.
- * Returns: the actual number of letters it could read and print
+ * Return: the actual number of letters it could read and print
  * if the file can not be opened or read, return 0
  * if filename is NULL return 0
  * if write fails or does not write
