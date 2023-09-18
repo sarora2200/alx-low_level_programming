7ff61ddd9dfa4569c314f037b558bed5ae77f220
sarora2200 <sarora2200@gmail.com>
2023-09-18 11:22:54 -0700

    done

--- 0x18-dynamic_libraries/test.c
+++ 0x18-dynamic_libraries/test.c
@@ -2,9 +2,16 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int printf(const char*format, ...)
+/**
+ * printf - function to print line
+ * @format: format.
+ * Return: 0 if success.
+*/
+
+
+int printf(const char *format, ...)
 {
 	write(1, "9 8 10 24 75 - 9\n", 17);
 	write(1, "Congratulations, you win the Jackpot!\n", 38);
 	exit(EXIT_SUCCESS);
-}	
+}
