2789b60dd0c5262d1220611a28091831ef1ffddb
sarora2200 <sarora2200@gmail.com>
2023-09-18 12:34:49 -0700

    done

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o myprintf.so -fPIC test.c
+wget -P /tmp  https://github.com/sarora2200/alx-low_level_programming/raw/1878c02e939c9080aaf196b2805fb9bc1b46c184/0x18-dynamic_libraries/myprintf.so
 export LD_PRELOAD=/tmp/myprintf.so
