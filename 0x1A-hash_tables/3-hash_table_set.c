4847553d969234ac5bf43b162eb18967d82f939f
sarora2200 <sarora2200@gmail.com>
2023-10-05 11:33:17 -0700

    done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,11 +2,11 @@
 
 
 /**
- * hash_table_set - function that adds an element to the hash table.
+ * hash_table_set - A function that adds an element to the hash table.
  * @ht: the hash table you want to add or update the key/value to
  * @key: the key.
  * @value: the value associated with the key.
- * Returns: 1 if it succeeded, 0 otherwise
+ * Return: 1 if it succeeded, 0 otherwise
 */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
