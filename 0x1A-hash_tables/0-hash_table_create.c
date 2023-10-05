6c27910b1d5564067b30f41d02a3bd57df6260a3
sarora2200 <sarora2200@gmail.com>
2023-10-05 10:31:12 -0700

    done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,8 +9,8 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-unsigned long  int i;
-hash_table_t *new_table = NULL;
+unsigned long  int i = 0;
+hash_table_t *new_hash_table = NULL;
 new_hash_table = malloc(sizeof(hash_table_t));
 if (!new_hash_table)
 	return (NULL);
@@ -21,7 +21,7 @@ if (!new_hash_table->array)
 	free(new_hash_table);
 	return (NULL);
 }
-for (i = 0; i < size; i++)
+for (; i < size; i++)
 	(new_hash_table->array)[i] = NULL;
 return (new_hash_table);
 }
