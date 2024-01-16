1f019f37751c1582a3b3b9947b25ba47ed7e7fbc
sarora2200 <sarora2200@gmail.com>
2024-01-16 07:46:47 -0800

    done

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,37 +1,50 @@
+#include <math.h>
 #include "search_algos.h"
 
 /**
- * jump_search - function that searches for a value
- * in a sorted array of integers
+ * jump_search - search a sorted array for a value using jump search method
+ * @array: the array to be searched
+ * @size: the size of the array (number of elements)
+ * @value: the int to be searched for
  *
- * @array: a pointer to the first element of the array to search in
- * @size:  the number of elements in array
- * @value: is the value to search for
- * Return: the first index where value is located
+ * Return: 1st index value is found at or -1 if not present or null array
  */
-
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i, step, jump;
+	int jump = sqrt(size);
+	int start = 0;
+	int section_end = 0;
 
-	if (array == NULL || size == 0)
+	if (array == NULL || size == 0 || array[start] > value)
 		return (-1);
 
-	step = sqrt(size);
-
-	for (i = jump = 0; jump < size && array[jump] < value;)
+	while (section_end < (int)size)
 	{
-		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
-		i = jump;
-		jump += step;
+		if (array[section_end] < value)
+		{
+			printf("Value checked array[%d] = [%d]\n", section_end, array[section_end]);
+
+			start = section_end;
+			section_end += jump;
+		}
+		else
+		{
+			break;
+		}
 	}
 
-	printf("Value found between indexes[%ld] and [%ld]\n", i, jump);
+	printf("Value found between indexes [%d] and [%d]\n", start, section_end);
 
-	jump = jump > size - 1 ? jump : size - 1;
-	for (; i < jump && array[i] < value; i++)
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	if (section_end > (int)size - 1)
+		section_end = size - 1;
 
-	return (array[i] == value ? (int)i : -1);
+	while (start <= section_end)
+	{
+		printf("Value checked array[%d] = [%d]\n", start, array[start]);
+		if (array[start] == value)
+			return (start);
+		start++;
+	}
+
+	return (-1);
 }
